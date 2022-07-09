07f885454cf21b7d215a58da7fcb907715e546bd
carloslvm <cvaldez553@gmail.com>
2022-07-09 13:47:56 -0400

    Integer comparison corrected

--- book_exercise/crash/Chapter 7: Input and While Loops/movie_tickets.py
+++ book_exercise/crash/Chapter 7: Input and While Loops/movie_tickets.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 
 years = 'Enter age: '
 active = True
@@ -7,9 +7,9 @@ while active:
     age = input(years)
     if age == exit: 
         active = False 
-    elif age < 3:
+    elif int(age) < 3:
         print('You can enter for free.')
-    elif age >= 3 and age <= 12:
+    elif int(age) >= 3 and int(age) <= 12:
         print('The ticket\'s value is $10')
-    elif age > 12:
+    elif int(age) > 12:
         print('The ticket\'s value is $15')
